eafb9131a0e5d4030884190dfafb4c08f5daf7d8
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 20:38:31 +0400

    add constracter

--- src/Data/Book.java
+++ src/Data/Book.java
@@ -4,10 +4,10 @@ public class Book extends LibraryItem {
 
     private String author;
 
-    //public Book(int newId, String newTitle, String newAuthor) {
-      //  super(newId, newTitle);
-        //author = newAuthor;
-    //}
+    public Book(int newId, String newTitle, String newAuthor) {
+       super(newId, newTitle);
+        author = newAuthor;
+    }
 
     public String getDetails() {
         return "Book: " + getTitle() + " by " + author;
