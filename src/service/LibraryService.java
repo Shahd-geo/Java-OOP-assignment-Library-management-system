940cc37ed90a3ede5d4052ebe4ad05cd792ee14e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 21:49:30 +0400

    add LibraryItem arraylist

--- src/service/LibraryService.java
+++ src/service/LibraryService.java
@@ -1,11 +1,26 @@
 package service;
 
-
 import Data.LibraryItem;
-
 import java.util.ArrayList;
 
 public class LibraryService {
+
     private ArrayList<LibraryItem> items;
 
+    public LibraryService() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(LibraryItem item) {
+        items.add(item);
+    }
+
+    public LibraryItem findItemById(int id) {
+        for (LibraryItem item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
