5fbc97f2fd1129bbc8315a46122840588da2eb59
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 21:25:00 +0400

    add variables in member class

--- src/Data/Member.java
+++ src/Data/Member.java
@@ -1,4 +1,10 @@
 package Data;
+import java.util.ArrayList;
+
 
 public class Member {
+    private int id;
+    private String name;
+    private Address address;
+    private ArrayList<LibraryItem> borrowedItems;
 }
