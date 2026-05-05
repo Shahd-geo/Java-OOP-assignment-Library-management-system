8f5bb793f570779c1c731def555ef311e83d613c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 20:37:51 +0400

    creat book class inherts from libararyiteam

--- src/Data/Book.java
+++ src/Data/Book.java
@@ -1,4 +1,15 @@
 package Data;
 
-public class Book {
+public class Book extends LibraryItem {
+
+    private String author;
+
+    //public Book(int newId, String newTitle, String newAuthor) {
+      //  super(newId, newTitle);
+        //author = newAuthor;
+    //}
+
+    public String getDetails() {
+        return "Book: " + getTitle() + " by " + author;
+    }
 }
