9b3769f075f1445cb727e819fc6e5c2f6f356069
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 20:31:11 +0400

    creat constracter +getter to inter private var

--- src/Data/LibraryItem.java
+++ src/Data/LibraryItem.java
@@ -5,5 +5,35 @@ public class LibraryItem {
         private String title;
         private boolean isAvailable;
 
+        public LibraryItem(int newId, String newTitle) {
+                IteamId = newId;
+                title = newTitle;
+                isAvailable = true;
+        }
 
+        public int getId() {
+                return IteamId;
+        }
+
+        public String getTitle() {
+                return title;
+        }
+
+        public boolean isAvailable() {
+                return isAvailable;
+        }
+
+        public void borrowItem() {
+                isAvailable = false;
+        }
+
+        public void returnItem() {
+                isAvailable = true;
+        }
+
+  //  public String getDetails() {
+     //   return null;
+   // }
 }
+
+
