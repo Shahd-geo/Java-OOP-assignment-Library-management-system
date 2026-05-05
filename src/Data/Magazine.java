bc6b475abac7baba6a7c035bd8a787719455016e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-05 20:48:04 +0400

    add issueNumber + constracter

--- src/Data/Magazine.java
+++ src/Data/Magazine.java
@@ -1,4 +1,11 @@
 package Data;
 
 public class Magazine extends LibraryItem {
-}
+
+        private int issueNumber;
+
+        public Magazine(int newId, String newTitle, int newIssue) {
+            super(newId, newTitle);
+            issueNumber = newIssue;
+        }
+
