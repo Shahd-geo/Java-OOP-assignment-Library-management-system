423c3472ffe996a0bc4a9ea40f843ad4b04f9a1f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 09:09:13 +0400

    add Library system Menu

--- src/MainApp.java
+++ src/MainApp.java
@@ -19,5 +19,14 @@ public class MainApp {
         while (true) {
 
             System.out.println("====== Library System ======");
+            System.out.println("1. Add Book");
+            System.out.println("2. Add Magazine");
+            System.out.println("3. Show Available Items");
+            System.out.println("4. Add Member");
+            System.out.println("5. Show Members");
+            System.out.println("6. Borrow Item");
+            System.out.println("7. Return Item");
+            System.out.println("8. Exit");
 
-    }
+
+        }
