d4b88e2bcc899b08e611ff8badcd0c244e5e7242
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 09:57:19 +0400

    add Library system Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,23 @@
+import service.BorrowService;
+import service.LibraryService;
+import service.MemberService;
+
 public class Menu {
-}
+
+        private LibraryService libraryService;
+
+        private MemberService memberService;
+
+        private BorrowService borrowService;
+
+        public Menu() {
+
+            libraryService = new LibraryService();
+
+            memberService = new MemberService();
+
+            borrowService =
+                    new BorrowService(libraryService, memberService);
+        }
+
+    }
