0e5d99d598487e11d1b434afdb79e2b5013ed8c3
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 09:02:02 +0400

    add brow iteam function

--- src/service/BorrowService.java
+++ src/service/BorrowService.java
@@ -1,5 +1,8 @@
 package service;
 
+import Data.LibraryItem;
+import Data.Member;
+
 public class BorrowService {
     private LibraryService libraryService;
     private MemberService memberService;
@@ -8,6 +11,28 @@ public class BorrowService {
 
         libraryService = newLibraryService;
         memberService = newMemberService;
+    }
+    public void borrowItem(int memberId, int itemId) {
+
+        Member member = memberService.findMemberById(memberId);
+
+        LibraryItem item = libraryService.findItemById(itemId);
+
+        if (member == null || item == null) {
+            System.out.println("Member or Item not found");
+            return;
+        }
+
+        if (!item.isAvailable()) {
+            System.out.println("Item already borrowed");
+            return;
+        }
+
+        item.borrowItem();
+
+        member.borrowItem(item);
+
+        System.out.println("Borrow successful");
     }
 
 
