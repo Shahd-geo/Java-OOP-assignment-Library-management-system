18a1fca93138f9459441a5a6916e18505123b08b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 08:59:38 +0400

    creat constractor

--- src/service/BorrowService.java
+++ src/service/BorrowService.java
@@ -3,5 +3,12 @@ package service;
 public class BorrowService {
     private LibraryService libraryService;
     private MemberService memberService;
+    public BorrowService(LibraryService newLibraryService,
+                         MemberService newMemberService) {
+
+        libraryService = newLibraryService;
+        memberService = newMemberService;
+    }
+
 
 }
