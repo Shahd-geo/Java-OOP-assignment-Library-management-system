ddec9fae2862570b87feba7336253f44c0dbbaa7
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 14:01:14 +0400

    creat menu clazss

--- src/Menu.java
+++ src/Menu.java
@@ -1,26 +1,30 @@
+import Data.Address;
+import Data.Magazine;
+import Data.Member;
 import service.BorrowService;
 import service.LibraryService;
 import service.MemberService;
-
+import Data.Book;
 import java.util.Scanner;
 
 public class Menu {
 
-        private LibraryService libraryService;
+    private LibraryService libraryService;
 
-        private MemberService memberService;
+    private MemberService memberService;
 
-        private BorrowService borrowService;
+    private BorrowService borrowService;
 
-        public Menu() {
+    public Menu() {
 
-            libraryService = new LibraryService();
+        libraryService = new LibraryService();
 
-            memberService = new MemberService();
+        memberService = new MemberService();
 
-            borrowService =
-                    new BorrowService(libraryService, memberService);
-        }
+        borrowService =
+                new BorrowService(libraryService, memberService);
+    }
+
     public void start() {
         Scanner input = new Scanner(System.in);
         boolean running = true;
@@ -39,8 +43,119 @@ public class Menu {
             System.out.println("8. Exit");
 
             int choice = input.nextInt();
+            switch (choice) {
+
+                case 1:
+
+                    System.out.println("Enter Book ID:");
+                    int id = input.nextInt();
+
+                    input.nextLine();
+
+                    System.out.println("Enter Book Title:");
+                    String title = input.nextLine();
+
+                    System.out.println("Enter Author Name:");
+                    String author = input.nextLine();
+
+                    Book book = new Book(id, title, author);
+
+                    libraryService.addItem(book);
+
+                    System.out.println("Book Added Successfully");
+
+                    break;
+                case 2:
+
+                    System.out.println("Enter Magazine ID:");
+                    int magId = input.nextInt();
+
+                    input.nextLine();
+
+                    System.out.println("Enter Magazine Title:");
+                    String magTitle = input.nextLine();
+
+                    System.out.println("Enter Issue Number:");
+                    int issue = input.nextInt();
+
+                    Magazine magazine =
+                            new Magazine(magId, magTitle, issue);
+
+                    libraryService.addItem(magazine);
+
+                    System.out.println("Magazine Added Successfully");
+
+                    break;
+                default:
+
+                    System.out.println("Invalid Option");
+            case 3:
+
+                libraryService.showAvailableItems();
+
+                break;
+                case 4:
+
+                    System.out.println("Enter Member ID:");
+                    int memberId = input.nextInt();
+
+                    input.nextLine();
+
+                    System.out.println("Enter Member Name:");
+                    String memberName = input.nextLine();
+
+                    System.out.println("Enter Street Name:");
+                    String street = input.nextLine();
+
+                    System.out.println("Enter City Name:");
+                    String city = input.nextLine();
+
+                    Address address =
+                            new Address(street, city);
+
+                    Member member =
+                            new Member(memberId, memberName, address);
+
+                    memberService.addMember(member);
+
+                    System.out.println("Member Added Successfully");
+
+                    break;
+                case 5:
+
+                    memberService.showAllMembers();
+
+                    break;
+                case 6:
+
+                    System.out.println("Enter Member ID:");
+                    int borrowMemberId = input.nextInt();
+
+                    System.out.println("Enter Item ID:");
+                    int borrowItemId = input.nextInt();
+
+                    borrowService.borrowItem(borrowMemberId, borrowItemId);
+
+                    break;
+                case 7:
+
+                    System.out.println("Enter Member ID:");
+                    int returnMemberId = input.nextInt();
+
+                    System.out.println("Enter Item ID:");
+                    int returnItemId = input.nextInt();
+
+                    borrowService.returnItem(returnMemberId, returnItemId);
+
+                    break;
+                case 8:
+
+                    running = false;
+
+                    System.out.println("Program Ended");
+
+                    break;
+        }
 
     }
-
-
-    }
+}
