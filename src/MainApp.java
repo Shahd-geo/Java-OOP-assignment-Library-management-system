d8f0140485c5219e299646f99a12ad82c35d709b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-06 09:07:30 +0400

    add services

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,8 +1,15 @@
+import service.LibraryService;
+import service.MemberService;
+
 import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        LibraryService libraryService = new LibraryService();
+
+        MemberService memberService = new MemberService();
 
     }
